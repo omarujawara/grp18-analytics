dfaa864a353c879483baaa3f27a8f315285b8353
Nelson Huang <nelson.huang@baruchmail.cuny.edu>
2026-04-26 03:29:23 +0000

    fixed minor syntax errors for the illegal parking dim tables

--- models/marts/illegal_parking_mart/dim_agency.sql
+++ models/marts/illegal_parking_mart/dim_agency.sql
@@ -1,17 +1,19 @@
 WITH agencies AS (
-    SELECT DISTINCT
-       agency AS agency
-       agency_name as agency_name
-    FROM {{ ref('stg_311_illegal_parking')}}
+   SELECT DISTINCT
+        agency AS agency,
+        agency_name AS agency_name
+   FROM {{ ref('stg_311_illegal_parking') }}
 ),
 
 final_agency_dim AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key([
-                'agency',
-                'agency_name'
-        ]) }} AS agency_key,
-    FROM agencies
+   SELECT
+       {{ dbt_utils.generate_surrogate_key([
+           'agency',
+           'agency_name'
+       ]) }} AS agency_key,
+       agency,
+       agency_name
+   FROM agencies
 )
 
-SELECT * FROM final_agency_dim 
+SELECT * FROM final_agency_dim
